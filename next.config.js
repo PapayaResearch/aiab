20cffbc3ab70f67961fd9473aa4b83c15710c6c2
Manuel Cherep <manuelcherep@gmail.com>
2023-02-01 14:38:49 -0500

    Minor change in Next config

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@
 const isProd = process.env.NODE_ENV === "production";
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = "";
-let basePath = "/";
+let assetPrefix = "/";
+let basePath = "";
 
 if (isGithubActions) {
     const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
