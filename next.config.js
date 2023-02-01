7ddfec0f65dde44aabdcb0757f21efa14bac6b1a
Manuel Cherep <manuelcherep@gmail.com>
2023-02-01 11:51:00 -0500

    Next deployment config

--- next.config.js
+++ next.config.js
@@ -1,32 +1,40 @@
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === "production";
-const path = process.env.NEXT_PUBLIC_DEPLOY_PATH;
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+    if (!repo.includes(".github.io")) {
+        assetPrefix = `/${repo}/`
+        basePath = `/${repo}`
+    }
+}
 
 const nextConfig = {
     reactStrictMode: true,
     trailingSlash: false,
-    assetPrefix: isProd ? path : undefined,
-    basePath: isProd ? path : undefined,
+    assetPrefix: isProd ? assetPrefix : undefined,
+    basePath: isProd ? basePath : undefined,
     swcMinify: true,
     webpack: (config) => {
         config.module.rules.push({
-            test: /\.(tex|bib|md)$/,
+            test: /\.md$/,
             use: "raw-loader"
         });
         config.module.rules.push({
             test: /\.ya?ml$/,
             use: "js-yaml-loader"
         });
+        config.module.rules.push({
+            test: /\.tex$/,
+            use: "raw-loader"
+        });
         return config;
-    },
-    async rewrites() {
-        return [
-            {
-                source: "/soundcloud-oembed",
-                destination: "https://soundcloud.com/oembed"
-            },
-        ]
     }
 }
 
