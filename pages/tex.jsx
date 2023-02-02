0daf4a709528676e40b1a289a3b17e88f7f4bb49
nikhilsinghmus <nikhilsinghmus@gmail.com>
2023-02-02 16:38:11 -0500

    Rendering and other updates.

--- pages/tex.jsx
+++ pages/tex.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { Text, Title, Anchor, Space, Box, Image, Center, Group, Table, HoverCard } from "@mantine/core";
+import { Text, Title, Anchor, Space, Box, Loader, Center, Group } from "@mantine/core";
 import { parseTeX } from "../lib/utils";
 import { printRaw } from "@unified-latex/unified-latex-util-print-raw";
 import paper from "../data/tex/main.tex";
 import { parseBibFile, normalizeFieldValue } from "bibtex";
 import { MathJax } from "better-react-mathjax";
+import { TeXTable, TeXFigure, TeXStripFigure, TeXAlgorithm, TeXCitation } from "../components/TeXEnvironments";
+
+export async function getStaticProps() { return { props: {} } }
 
 const TeX = () => {
     const [parsed, setParsed] = useState([]);
@@ -59,40 +62,12 @@ const TeX = () => {
                         return <span key={key} id={labelId}></span>;
                     case "times": return <span key={key}>x</span>;
                     case "years": return node.args[0].content.length ? <Box key={key} pt={"xs"} style={{width: 100, display: "inline-block", transform: "translate(-125px, -9px)", position: "absolute"}}><Text size={"sm"} align={"right"}><b>{node.args[0].content.map(render)}</b></Text></Box> : null;
-                    case "oplus": return "⊕";
-                    case "tilde": return "~";
-                    case "epsilon": return "ε";
-                    case "sigma": return "σ";
-                    case "mu": return "μ";
-                    case "lambda": return "λ";
-                    case "beta": return "ß";
-                    case "eta": return "η";
-                    case "lvert": case"rvert": return "|";
-                    case "log": return "log";
                     case "KwIn": return <Title key={key} order={6}><br/>In:</Title>;
                     case "KwOut": return <Title key={key} order={6}><br/>Out:</Title>;
                     case "For": return <Title key={key} display={"inline"} order={6}><br/>For </Title>;
                     case "^": return <sup key={key}>{node.args[0].content.map(render)}</sup>;
                     case "hline": return undefined;
-                    case "cite":
-                        const ref = node.args[1].content[0].content.toLowerCase();
-                        if (!bib[ref]) {
-                            return undefined;
-                        }
-                        const n = bib[ref].i;
-                        const entry = bib[ref].entry;
-                        return <HoverCard key={key}>
-                            <HoverCard.Target>
-                                <Text display={"inline"}><Anchor href={"#ref:" + n}>{"[" + n + "]"}</Anchor></Text>
-                            </HoverCard.Target>
-                            <HoverCard.Dropdown>
-                                <Box p={"md"} style={{width: 400}}>
-                                    <Title order={6}><b>{normalizeFieldValue(entry.fields.title)}</b></Title>
-                                    <Text size={"sm"}><b>{normalizeFieldValue(entry.fields.author) + "."}</b></Text>
-                                    <Text size={"sm"}><b>{normalizeFieldValue(entry.fields.year)}</b></Text>
-                                </Box>
-                            </HoverCard.Dropdown>
-                        </HoverCard>;
+                    case "cite": return <TeXCitation key={key} node={node} bib={bib}/>
                     case "ref":
                         const refName = node.args[node.args.length - 1].content.map((x) => x.content).join("");
                         const refType = refName.split(":")[0];
@@ -101,7 +76,6 @@ const TeX = () => {
                             return <Anchor key={key} href={"#" + refName}>{refSet[refName]}</Anchor>
                         }
                     default: console.log(node); return undefined;
-
                 }
             case "string": return <Text key={key} display={"inline"}>{node.content}</Text>;
             case "whitespace": return " ";
@@ -113,79 +87,12 @@ const TeX = () => {
                 switch (node.env) {
                     case "itemize": return <ul key={key}>{node.args.map((x, i) => <li key={i}>{x.content.map(render)}</li>)}</ul>;
                     case "enumerate": return <ol key={key}>{node.args.map((x, i) => <li key={i}>{x.content.map(render)}</li>)}</ol>;
-                    case "figure": case "figure*":
-                        const fig = node.content.filter((x) => x.content === "includegraphics")[0].args.filter((x) => x.openMark === "{")[0].content.map((x) => x.content).join("");
-                        const width = node.content.filter((x) => x.content === "includegraphics")[0].args.filter((x) => x.openMark === "[")[0].content;
-                        const wScale = (width.length == 4) ? +width[2].content : 1;
-                        const caption = node.content.filter((x) => x.content === "caption")[0].args.filter((x) => x.openMark === "{")[0].content.map(render);
-                        const figName = node.content.filter((x) => x.content === "label")[0].args.filter((x) => x.openMark === "{")[0].content.map((x) => x.content).join("");
-                        return <Center key={key} id={figName}>
-                                <Box pb={"xl"} pt={"sm"} style={{maxWidth: `calc(${node.env === "figure*" ? "100%" : "50%"} * ${wScale})`}}>
-                                    <Image src={"/tex/" + fig} alt={fig}/>
-                                    <Text size={"sm"} color={"dimmed"} align={"justify"}><b>Figure {figures[figName]}. </b>{caption}</Text>
-                                </Box>
-                            </Center>;
-                     case "strip":
-                        const content = node.content.filter((x) => x.content === "includegraphics")[0].args.filter((x) => x.openMark === "{")[0].content.map((x) => x.content).join("");
-                        const sCaption = node.content[6].content.map(render);
-                        const sName = node.content.filter((x) => x.content === "label")[0].args.filter((x) => x.openMark === "{")[0].content.map((x) => x.content).join("");
-
-                        return <Center key={key} id={sName}>
-                                <Box pb={"xl"} pt={"sm"} style={{maxWidth: "100%"}}>
-                                    <Image src={"/tex/" + content} alt={content}/>
-                                    <Text size={"sm"} color={"dimmed"} align={"justify"}><b>Figure {figures[sName]}. </b>{sCaption}</Text>
-                                </Box>
-                            </Center>;
-                    case "table": case "table*":
-                        const tabular = node.content.filter((x) => x.env === "tabular")[0];
-                        const numCols = tabular.args[1].content[0].content.replace("|", "").length;
-                        const tabularFull = tabular.content
-                            .map((x, i) => (x.type === "string" || x.content === "\\") ? x.content : render(x, i))
-                            .filter((x) => x && x !== " ");
-                        let cells= [];
-                        var lastI = 0;
-                        const tabName = node.content.filter((x) => x.content === "label")[0].args.filter((x) => x.openMark === "{")[0].content.map((x) => x.content).join("");
-                        const tabCaption = node.content.filter((x) => x.content === "caption")[0].args.filter((x) => x.openMark === "{")[0].content.map(render);
-
-                        for (let i = 0; i < tabularFull.length; i += 1) {
-                            if (["&", "\\"].includes(tabularFull[i])) {
-                                const cell = tabularFull.slice(lastI, i).map((x, j) => x.content ? render(x, j) : x);
-                                cells = [...cells, cell];
-                                lastI = i + 1;
-                            }
-                        }
-                        cells = [...cells, tabularFull.slice(lastI, tabularFull.length).map((x, j) => x.content ? render(x, j) : x)];
-
-                        let rows= [];
-                        for (let i = 0; i < cells.length; i += numCols) {
-                            const chunk = cells.slice(i, i + numCols);
-                            rows = [...rows, chunk];
-                        }
-                        return <Center key={key} id={tabName}>
-                                <Box pb={"xl"} pt={"sm"} style={{maxWidth: node.env === "table*" ? "100%" : "50%"}}>
-                                    <Text size={"sm"} color={"dimmed"} align={"justify"}><b>Table {tables[tabName]}. </b>{tabCaption}</Text>
-                                    <Table pt={"xl"}>
-                                        <thead>
-                                            <tr>{rows[0].map((x, i) => <th key={i}>{x}</th>)}</tr>
-                                        </thead>
-                                        <tbody>
-                                            {rows.slice(1).map((x, i) => <tr key={i}>{x.map((y, j) => <td key={j}>{y}</td>)}</tr>)}
-                                        </tbody>
-                                    </Table>
-                                </Box>
-                            </Center>
+                    case "figure": case "figure*": return <TeXFigure key={key} node={node} figures={figures} render={render}/>;
+                    case "strip": return <TeXStripFigure key={key} node={node} figures={figures} render={render}/>;
+                    case "table": case "table*": return <TeXTable key={key} node={node} tables={tables} render={render}/>;
+                    case "algorithm": return <TeXAlgorithm key={key} node={node} algorithms={algorithms} render={render}/>;
                     case "abstract":
                         return <Center key={key}><Box pt={"xl"} pb={"xl"} style={{maxWidth: "90%"}}><Text size={"sm"}><b>Abstract. </b>{node.content.map(render)}</Text></Box></Center>;
-                    case "algorithm":
-                        const aCaption = node.content.filter((x) => x.content === "caption")[0].args.filter((x) => x.openMark === "{")[0].content.map(render);
-                        const algoName = node.content.filter((x) => x.content === "label")[0].args.filter((x) => x.openMark === "{")[0].content.map((x) => x.content).join("");
-                        return <Center key={key}>
-                            <Box style={{maxWidth: 800, backgroundColor: "rgba(120, 120, 120, 0.2)", padding: 10, marginBottom: 20, borderRadius: 8}}>
-                                <Text size={"sm"} color={"dimmed"} align={"justify"}><b>Algorithm {algorithms[algoName]}. </b>{aCaption}</Text>
-                                <br/>
-                                <Text size={"sm"}>{node.content.map(render)}</Text>
-                            </Box>
-                        </Center>;
                     default: console.log(node); return undefined;
                 }
             case "mathenv":
@@ -216,8 +123,8 @@ const TeX = () => {
             });
     }, []);
 
-    if (!bib) {
-        return null;
+    if (!bib || !parsed || !Object.keys(bib).length || !Object.keys(parsed).length) {
+        return <Center pt={400}><Loader sx={(theme) => ({color: theme.colors[theme.primaryColor][6]})}/></Center>;
     }
 
     return (
